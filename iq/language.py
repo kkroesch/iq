cc8b41421e2e31fde6a9d7fbf5084f1f2e82b861
Karsten Kroesch <karsten.kroesch@ag.ch>
2023-12-25 19:28:53 +0100

    Layout improvements.

--- iq/language.py
+++ iq/language.py
@@ -1,4 +1,13 @@
+""" Filter for Jinja-Templates
+"""
 
+import arrow
+
+
+def humanize(date):
+    then = arrow.get(date)
+    now = arrow.now()
+    return then.humanize(then, locale='de')
 
 
 def pluralize(count, singular, plural=None):
@@ -6,3 +15,8 @@ def pluralize(count, singular, plural=None):
         return singular
     else:
         return plural if plural else singular + 's'
+
+
+def decimal(value, precision=2):
+    format_string = f'{{:.{precision}f}}'
+    return format_string.format(value)
--- iq/server.py
+++ iq/server.py
@@ -2,14 +2,22 @@ from fastapi import FastAPI, Request, Query
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from jinja2 import Environment, FileSystemLoader
+
 
 from iq import es
-from iq.language import pluralize
+from iq.language import pluralize, decimal, humanize
 
 app = FastAPI()
+env = Environment(loader=FileSystemLoader('templates'))
+env.filters['pluralize'] = pluralize
+env.filters['decimal'] = decimal
+env.filters['humanize'] = humanize
+
 templates = Jinja2Templates(directory="templates")
-templates.env.filters['pluralize'] = pluralize 
-#app.mount("/static", StaticFiles(directory="static"), name="static")
+templates.env = env 
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 def pluralize(count, singular, plural=None):
@@ -33,7 +41,7 @@ async def search(request: Request, query: str = Query(None)):
                 "query": query
             }
         },
-        "_source": ["title", "url"],
+        "_source": ["title", "url", "last_seen"],
         "highlight": {
             "fields": {
                 "paragraphs": {}
