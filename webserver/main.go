96ddb355f46c8e43dc00be885720a4ca2e4f79a5
Karsten Kroesch <karsten.kroesch@ag.ch>
2024-04-09 15:29:44 +0200

    inlined searchbox

--- webserver/main.go
+++ webserver/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+type Query struct {
+	Keyword string `form:"query"`
+}
 
 func main() {
 	gin.SetMode(gin.DebugMode)
@@ -15,10 +20,19 @@ func main() {
 	router.StaticFS("/static", http.Dir("./static"))
 
 	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
+	router.GET("/q", func(c *gin.Context) {
 		hits := []string{"Apfel", "Banane", "Kirsche"}
+		var q Query
+		c.Bind(&q)
+		fmt.Print("Search for", q.Keyword)
+
 		// HTML-Template rendern und Daten übergeben
 		c.HTML(http.StatusOK, "index.html", gin.H{
-			"Hits": hits,
+			"Keyword": q.Keyword,
+			"Hits":    hits,
 		})
 	})
 
