1b1f79f92f1b117cd6adbfab4c4ae1143ab8bf18
Karsten Kroesch <karsten.kroesch@ag.ch>
2024-04-09 16:33:39 +0200

    Fixed filter functions.

--- webserver/main.go
+++ webserver/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+	"time"
 
+	"github.com/dustin/go-humanize"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,11 +21,24 @@ type Hit struct {
 	Indexed uint32
 }
 
+func time_humanize(timestamp uint32) string {
+	t := time.Unix(int64(timestamp), 0)
+	return humanize.Time(t)
+}
+
+func safeHTML(htmlContent string) template.HTML {
+	return template.HTML(htmlContent)
+}
+
 func main() {
 	gin.SetMode(gin.DebugMode)
 	router := gin.Default()
 
 	// Template laden
+	router.SetFuncMap(template.FuncMap{
+		"time_humanize": time_humanize,
+		"safe_html":     safeHTML,
+	})
 	router.LoadHTMLGlob("templates/*")
 	router.StaticFS("/static", http.Dir("./static"))
 
@@ -31,12 +46,12 @@ func main() {
 		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
 
+	hits := []Hit{{"Apfel", "http://apfel.com", 0.98, "Ein <em>Apfel</em> am Tag hält den Arzt fern", 1712670481}, {"Banane", "http://banana.com", 0.98, "Ein <em>Apfel</em> ist nicht so krumm wie eine Banane", 1712670481}}
+
 	router.GET("/q", func(c *gin.Context) {
-		hits := []Hit{{"Apfel", "http://apfel.com", 0.98, "Ein <em>Apfel</em> am Tag hält den Arzt fern", 1712670481}, {"Banane", "http://banana.com", 0.98, "Ein <em>Apfel</em> ist nicht so krumm wie eine Banane", 1712670481}}
 
 		var q Query
 		c.Bind(&q)
-		fmt.Print("Search for", q.Keyword)
 
 		// HTML-Template rendern und Daten übergeben
 		c.HTML(http.StatusOK, "index.html", gin.H{
