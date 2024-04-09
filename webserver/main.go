dcd7fd4d5c05e78e65917e5297902247916435c1
Karsten Kroesch <karsten.kroesch@ag.ch>
2024-04-09 15:52:37 +0200

    Added Hit struct.

--- webserver/main.go
+++ webserver/main.go
@@ -11,6 +11,14 @@ type Query struct {
 	Keyword string `form:"query"`
 }
 
+type Hit struct {
+	Title   string
+	Url     string
+	Score   float32
+	Hilite  string
+	Indexed uint32
+}
+
 func main() {
 	gin.SetMode(gin.DebugMode)
 	router := gin.Default()
@@ -24,7 +32,8 @@ func main() {
 	})
 
 	router.GET("/q", func(c *gin.Context) {
-		hits := []string{"Apfel", "Banane", "Kirsche"}
+		hits := []Hit{{"Apfel", "http://apfel.com", 0.98, "Ein <em>Apfel</em> am Tag hält den Arzt fern", 1712670481}, {"Banane", "http://banana.com", 0.98, "Ein <em>Apfel</em> ist nicht so krumm wie eine Banane", 1712670481}}
+
 		var q Query
 		c.Bind(&q)
 		fmt.Print("Search for", q.Keyword)
